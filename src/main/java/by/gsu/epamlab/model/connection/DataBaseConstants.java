87ba6fa24f9d104a350de40bf7e937cb16c708c3
Maksim Ruts <gambitns@gmail.com>
2015-07-31 19:02:17 +0300

    рефакторинг констант

--- src/main/java/by/gsu/epamlab/model/connection/DataBaseConstants.java
+++ src/main/java/by/gsu/epamlab/model/connection/DataBaseConstants.java
@@ -17,7 +17,6 @@ public class DataBaseConstants {
         public static final String NAME = "name";
         public static final String TASK_DESCRIPTION = "description";
         public static final String TASK_DATE_ENDING = "date_ending";
-        public static final String TASK_TIME_ENDING = "time_ending";
         public static final String TASK_COMPLETED = "completed";
         public static final String TASK_DELETED = "deleted";
         public static final String FILEPATH = "filepath";
@@ -32,9 +31,9 @@ public class DataBaseConstants {
         public static final String SELECT_TASK = "select * from tasks where user_id = ? and id = ?";
         public static final String UPDATE_TASK = "update tasks set name = ?, description = ?, date_ending = ?, completed = ?, deleted = ?, filepath = ? where user_id = ? and id = ?";
         public static final String DELETE_TASK = "delete from tasks where user_id = ? and name = ?";
-        public static final String SELECT_ALL_TASKS = "select * from tasks where user_id = ?";
-        public static final String SELECT_TASKS_BY_TYPES = "select * from tasks where user_id = ? and completed = ? and deleted = ?";
         public static final String SELECT_TASKS_BY_TYPES_AND_DATA = "select * from tasks where user_id = ? and completed = ? and deleted = ? and date_ending <= ? and date_ending >= ?";
-        public static final String SELECT_TASKS_BY_TYPES_AND_DATA_AFTER = "select * from tasks where user_id = ? and completed = ? and deleted = ? and date_ending > ?";
+        public static final String SELECT_TASKS_BY_TYPES = "select * from tasks where user_id = ? and completed = ? and deleted = ?";
+        public static final String SELECT_TASKS_DELETED = "select * from tasks where user_id = ? and deleted = ?";
+        public static final String SELECT_ALL_TASKS = "select * from tasks where user_id = ?";
     }
 }
--- src/main/java/by/gsu/epamlab/model/daoimpl/database/StatementFactory.java
+++ src/main/java/by/gsu/epamlab/model/daoimpl/database/StatementFactory.java
@@ -1,36 +1,38 @@
 package by.gsu.epamlab.model.daoimpl.database;
 
 import by.gsu.epamlab.model.beans.TaskType;
+import by.gsu.epamlab.model.connection.DataBaseConstants;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class StatementFactory {
+
     public static PreparedStatement getInstanceForTasks(TaskType taskType, int userId, Connection connection) throws SQLException {
         PreparedStatement stmt;
         switch (taskType) {
             case TODAY:
             case TOMORROW:
             case SOMEDAY:
-                stmt = connection.prepareStatement("select * from tasks where user_id = ? and completed = ? and deleted = ? and date_ending <= ? and date_ending >= ?");
+                stmt = connection.prepareStatement(DataBaseConstants.Queries.SELECT_TASKS_BY_TYPES_AND_DATA);
                 stmt.setBoolean(2, taskType.isCompleted());
                 stmt.setBoolean(3, taskType.isDeleted());
                 stmt.setDate(4, taskType.getDateHigher());
                 stmt.setDate(5, taskType.getDateBelow());
                 break;
             case COMPLETE:
-                stmt = connection.prepareStatement("select * from tasks where user_id = ? and completed = ? and deleted = ?");
+                stmt = connection.prepareStatement(DataBaseConstants.Queries.SELECT_TASKS_BY_TYPES);
                 stmt.setBoolean(2, true);
                 stmt.setBoolean(3, false);
                 break;
             case RECYCLE_BIN:
-                stmt = connection.prepareStatement("select * from tasks where user_id = ? and deleted = ?");
+                stmt = connection.prepareStatement(DataBaseConstants.Queries.SELECT_TASKS_DELETED);
                 stmt.setBoolean(2, true);
                 break;
             case ALL:
             default:
-                stmt = connection.prepareStatement("select * from tasks where user_id = ?");
+                stmt = connection.prepareStatement(DataBaseConstants.Queries.SELECT_ALL_TASKS);
                 break;
         }
         stmt.setInt(1, userId);
